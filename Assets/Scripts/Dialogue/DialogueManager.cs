2c27fd45e72cf925a89e5c9f6c24d9940dfe9e67
Riximus <riximuslol@gmail.com>
2024-02-14 23:34:17 +0100

    feat: partially added dialogue system, still WIP

--- Assets/Scripts/Dialogue/DialogueManager.cs
+++ Assets/Scripts/Dialogue/DialogueManager.cs
@@ -1,9 +1,128 @@
-﻿using UnityEngine;
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Input;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UIElements;
+using Cursor = UnityEngine.Cursor;
 
 namespace Dialogue
 {
     public class DialogueManager : MonoBehaviour
     {
+        [SerializeField] private GameObject dialogueParent;
+        [SerializeField] private float typingSpeed = 0.05f;
+        [SerializeField] private float turnSpeed = 2f; // The speed at which the player turns to face the NPC
+
+        [Header("Player")]
+        [SerializeField] private PlayerController playerController; // TODO: Reference it differently
+        [SerializeField] private Transform playerCamera; // TODO: Reference it differently
+
+        private List<DialogueString> _dialogueList;
+        private Transform _playerCamera;
+
+        private int _currentDialogueIndex = 0;
         
+        private Button _option1Button;
+        private Button _option2Button;
+        private Label _dialogueText;
+
+        private void OnEnable()
+        {
+            VisualElement root = GetComponent<UIDocument>().rootVisualElement;
+            
+            _option1Button = root.Q<Button>("option-1");
+            _option2Button = root.Q<Button>("option-2");
+            _dialogueText = root.Q<Label>("dialogue-text");
+        }
+
+        private void Start()
+        {
+            dialogueParent.SetActive(false);
+        }
+
+        public void DialogueStart(List<DialogueString> dialogueStrings, Transform npc)
+        {
+            dialogueParent.SetActive(true);
+            playerController.enabled = false;
+            
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+
+            StartCoroutine(TurnCameraTowardsNpc(npc));
+            
+            _dialogueList = dialogueStrings;
+            _currentDialogueIndex = 0;
+
+            DisableButtons();
+            
+            StartCoroutine(PrintDialogue());
+        }
+
+        private void DisableButtons()
+        {
+            _option1Button.SetEnabled(false);
+            _option2Button.SetEnabled(false);
+            
+            _option1Button.visible = false;
+            _option2Button.visible = false;
+        }
+
+        private IEnumerator TurnCameraTowardsNpc(Transform npc)
+        {
+            Quaternion startRotation = playerCamera.rotation;
+            Quaternion targetRotation = Quaternion.LookRotation(npc.position - playerCamera.position);
+            
+            float elapsedTime = 0;
+            while (elapsedTime < 1f)
+            {
+                playerCamera.rotation = Quaternion.Slerp(startRotation, targetRotation, elapsedTime);
+                elapsedTime += Time.deltaTime * turnSpeed;
+                yield return null;
+            }
+            
+            playerCamera.rotation = targetRotation;
+        }
+
+        private bool _optionSelected = false;
+        private IEnumerator PrintDialogue()
+        {
+            while (_currentDialogueIndex < _dialogueList.Count)
+            {
+                DialogueString line = _dialogueList[_currentDialogueIndex];
+                
+                line.startDialogueEvent?.Invoke();
+                
+                if (line.isQuestion)
+                {
+                    yield return StartCoroutine(TypeText(line.text));
+
+                    _option1Button.SetEnabled(true);
+                    _option2Button.SetEnabled(true);
+                    
+                    _option1Button.text = line.answerOption1;
+                    _option2Button.text = line.answerOption2;
+                    
+                    _option1Button.visible = true;
+                    _option2Button.visible = true;
+                    
+                    _option1Button.clicked += () => HandleOptionSelected(line.nextDialogue1);
+                    _option2Button.clicked += () => HandleOptionSelected(line.nextDialogue2);
+
+                    yield return new WaitUntil(() => _optionSelected);
+                }
+                else
+                {
+                    yield return StartCoroutine(TypeText(line.text));
+                }
+                
+                line.endDialogueEvent?.Invoke();
+                
+                _optionSelected = false;
+            }
+
+            DialogueStop(); // TODO: https://youtu.be/BEaOHWkZhtE?si=12I340BuZDSiqUw7&t=1224
+        }
     }
 }
--- Assets/Scripts/Dialogue/DialogueTrigger.cs
+++ Assets/Scripts/Dialogue/DialogueTrigger.cs
@@ -1,13 +1,56 @@
-﻿using Input;
+﻿using System;
+using System.Collections.Generic;
+using Input;
 using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.Serialization;
 
 namespace Dialogue
 {
     public class DialogueTrigger : MonoBehaviour, IInteractable
     {
+        [SerializeField] private List<DialogueString> dialogueStrings = new List<DialogueString>();
+        [SerializeField] private Transform npcTransform;
+
+        private bool _hasSpoken = false;
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.CompareTag("Player") && !_hasSpoken)
+            {
+                other.gameObject.GetComponent<DialogueManager>().DialogueStart(dialogueStrings, npcTransform);
+                _hasSpoken = true;
+            }
+        }
+
         public void Interact()
         {
             Debug.Log("Interacting with dialogue trigger");
         }
+    }
+    
+    [System.Serializable]
+    public class DialogueString
+    {
+        public string text; // The text to display what the NPC is saying
+        public bool isEndOfDialogue; // Whether or not this is the end of the dialogue
+        
+        [Header("Branch")]
+        public bool isQuestion; // Whether or not this is a question
+        
+        // Trying out these ones to see
+        public string[] answerOptions; // The options for the player to choose from
+        public DialogueString[] nextDialogue; // The next dialogue to display based on the player's choice
+
+        public string answerOption1;
+        public string answerOption2;
+        
+        public DialogueString nextDialogue1; // The next dialogue to display based on answerOption1
+        public DialogueString nextDialogue2; // The next dialogue to display based on answerOption2
+        
+        [Header("Triggered Events")]
+        public UnityEvent startDialogueEvent; // The event to trigger when the dialogue starts
+        public UnityEvent endDialogueEvent; // The event to trigger when the dialogue ends
+
     }
 }
