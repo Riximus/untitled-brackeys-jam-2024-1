569bdb099ab1850eaff41628f05ad5e8adfada0c
g-hi3 <marc.forrer@bluewin.ch>
2024-02-17 19:19:48 +0100

    added a component that acts as an item

---
+++ Assets/Scripts/Inventory/InteractableItem.cs
@@ -0,0 +1,18 @@
+﻿using Input;
+using UnityEngine;
+
+namespace Inventory
+{
+    public class InteractableItem : MonoBehaviour, IInteractable
+    {
+        [SerializeField] private InventoryHandler inventoryHandler;
+        [SerializeField] private ItemKind itemKind;
+        
+        /// <inheritdoc />
+        public void Interact()
+        {
+            inventoryHandler.AddToInventory(itemKind);
+            Destroy(gameObject);
+        }
+    }
+}
