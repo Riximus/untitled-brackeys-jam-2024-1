5f63c030a44a699df2208dc824fb34dabe682f35
Riximus <riximuslol@gmail.com>
2024-02-18 11:24:26 +0100

    feat: changed ending trigger to rotate player

--- Assets/Scripts/EndingTrigger.cs
+++ Assets/Scripts/EndingTrigger.cs
@@ -1,4 +1,5 @@
-﻿using Input;
+﻿using System.Collections;
+using Input;
 using UnityEngine;
 
 namespace DefaultNamespace
@@ -8,21 +9,38 @@ namespace DefaultNamespace
         [SerializeField] private Transform carpenter;
         [SerializeField] private Transform musician;
         [SerializeField] private Transform librarian;
-        
+        [SerializeField] private Transform player;
+        [SerializeField] private float rotationSpeed = 2f; // Adjust rotation speed as needed
+
         public void Interact()
         {
-            Vector3 rotation = new Vector3(0, -108.276f, 0);
-            Vector3 librarianRotation = new Vector3(0, -118.032f, 0);
-
+            // Existing code for setting positions and rotations
             carpenter.position = new Vector3(-226.821f, 4.27f, 221.62f);
-            carpenter.eulerAngles = rotation;
+            carpenter.eulerAngles = new Vector3(0, -108.276f, 0);
 
             musician.position = new Vector3(-227.179f, 4.134f, 222.6f);
-            musician.eulerAngles = rotation;
+            musician.eulerAngles = new Vector3(0, -108.276f, 0);
 
             librarian.position = new Vector3(-227.2133f, 4.321f, 223.7123f);
-            librarian.eulerAngles = librarianRotation;
+            librarian.eulerAngles = new Vector3(0, -118.032f, 0);
+
+            // Start rotating the player towards the musician
+            StartCoroutine(RotateTowards(musician.position));
         }
 
+        private IEnumerator RotateTowards(Vector3 targetPosition)
+        {
+            Vector3 directionToTarget = targetPosition - player.position;
+            Quaternion targetRotation = Quaternion.LookRotation(directionToTarget);
+
+            int counter = 0;
+            
+            while (Quaternion.Angle(player.rotation, targetRotation) > 1f && counter < 200)
+            {
+                player.rotation = Quaternion.Slerp(player.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+                counter++;
+                yield return null;
+            }
+        }
     }
 }
