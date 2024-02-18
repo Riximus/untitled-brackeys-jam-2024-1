5b071e55c2938259ab5ed39ed14bda4e46a746f1
Riximus <riximuslol@gmail.com>
2024-02-18 10:57:39 +0100

    feat: added ending trigger with npc position changes
    player rotation is missing, needs slow rotation

---
+++ Assets/Scripts/EndingTrigger.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public class EndingTrigger : MonoBehaviour
+    {
+        [SerializeField] private Transform carpenter;
+        [SerializeField] private Transform musician;
+        [SerializeField] private Transform librarian;
+        
+        public void MoveAllVillagers()
+        {
+            Vector3 rotation = new Vector3(0, -108.276f, 0);
+            Vector3 librarianRotation = new Vector3(0, -118.032f, 0);
+
+            carpenter.position = new Vector3(-226.821f, 4.27f, 221.62f);
+            carpenter.eulerAngles = rotation;
+
+            musician.position = new Vector3(-227.179f, 4.134f, 222.6f);
+            musician.eulerAngles = rotation;
+
+            librarian.position = new Vector3(-227.2133f, 4.321f, 223.7123f);
+            librarian.eulerAngles = librarianRotation;
+        }
+
+    }
+}
